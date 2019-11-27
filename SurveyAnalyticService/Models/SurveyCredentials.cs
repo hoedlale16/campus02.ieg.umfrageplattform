285e56711a6385ffb036a1d67f4877d88be9bf3a
Patrik R <Patrik.Rauche@gmail.com>
2019-11-27 13:35:12 +0100

    Implement Collector + Analytics

---
+++ SurveyAnalyticService/Models/SurveyCredentials.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SurveyAnalyticService.Models
+{
+    public class SurveyCredentials
+    {
+        public string User { get; set; }
+        public string Password { get; set; }
+
+        public SurveyCredentials()
+        {
+        }
+
+        public SurveyCredentials(String user, String password)
+        {
+            this.User = user;
+            this.Password = password;
+        }
+
+        public SurveyCredentials(String json)
+        {
+            JObject jObject = JObject.Parse(json);
+            User = (string)jObject["user"];
+            Password = (string)jObject["password"];
+        }
+    }
+}
