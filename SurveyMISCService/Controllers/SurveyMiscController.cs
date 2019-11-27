28c272ebd5ceef4618a7978a6fc2a5e7740b3864
Alexander H <alexander.hoedl@edu.fh-joanneum.at>
2019-11-27 10:50:58 +0100

    A3 - SurveyMiscService - Consul Integration

---
+++ SurveyMISCService/Controllers/SurveyMiscController.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SurveyMISCService.Model;
+
+namespace SurveyMISCService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [FormatFilter]
+    public class SurveyMiscController : ControllerBase
+    {
+        private static readonly HttpClient _client = new HttpClient();
+        private static string _consulURL = "http://localhost:8500";
+
+        private ILogger<SurveyMiscController> _logger;
+        public SurveyMiscController(ILogger<SurveyMiscController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("health")]
+        public ActionResult GetHealthState()
+        {
+            return Ok();
+        }
+
+        [HttpGet("{serviceID}")]
+        public IActionResult Get(string serviceID)
+        {
+            List<string> _availableServiceUrls = null;
+
+            //Call Consul stuff to retreive serviceurls
+            _availableServiceUrls = GetUrlsForService(serviceID).Result;
+
+            if(_availableServiceUrls != null && _availableServiceUrls.Count > 0)
+            {
+                return Ok(_availableServiceUrls);
+            }
+
+            return BadRequest();
+        }
+
+        private async Task<List<string>> GetUrlsForService(String serviceID)
+        {
+            List<string> urls = new List<string>();
+            try
+            {
+                _client.DefaultRequestHeaders.Accept.Clear();
+                var filterPassing = "/v1/health/state/passing?filter=ServiceID==";
+                //http://127.0.0.1:8500/v1/health/state/passing?filter=ServiceID==SurveyQuestionService
+                HttpResponseMessage response = await _client.GetAsync(_consulURL + filterPassing + serviceID);
+                if (response.IsSuccessStatusCode)
+                {
+
+                    List<ConsulServicesState> states = await response.Content.ReadAsAsync<List<ConsulServicesState>>();
+                    foreach (var state in states)
+                    {
+                        if (state.Name != null)
+                        {
+                            urls.Add(state.Name);
+                        }
+                    }
+                }
+            }
+            catch (HttpRequestException ex)
+            {
+                _logger.LogError("An error occurred connecting to SimpleCreditCartServiceClient");
+            }
+
+            //Fallback return empty list
+            return urls;
+        }
+    }
+}
---
+++ SurveyMISCService/Model/ConsulServicesState.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SurveyMISCService.Model
+{
+    public class ConsulServicesState
+    {
+        public ConsulServicesState()
+        {
+        }
+        public ConsulServicesState(string json)
+        {
+            JObject jObject = JObject.Parse(json);
+            Node = (string)jObject["Node"];
+            Name = (string)jObject["Name"];
+            CheckID = (string)jObject["CheckID"];
+            Status = (string)jObject["Status"];
+            Notes = (string)jObject["Notes"];
+            Output = (string)jObject["Output"];
+            ServiceID = (string)jObject["ServiceID"];
+            ServiceName = (string)jObject["ServiceName"];
+        }
+
+        public string Node { get; set; }
+        public string Name { get; set; }
+        public string CheckID { get; set; }
+        public string Status { get; set; }
+        public string Notes { get; set; }
+        public string Output { get; set; }
+        public string ServiceID { get; set; }
+        public string ServiceName { get; set; }
+    }
+}
